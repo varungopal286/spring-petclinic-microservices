f9fab46c6ac9adb3bd8d8df2d59d364b60d1cb76
Varun Gopal <varungopal286@gmail.com>
2026-05-05 14:59:20 +0530

    fix: enable auto-assign public IP on subnets for EKS nodes

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,8 +8,9 @@ module "vpc" {
   azs            = ["${var.region}a", "${var.region}b"]
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  map_public_ip_on_launch = true
 
   # Required tags so EKS and ALB controller can find these subnets
   public_subnet_tags = {
